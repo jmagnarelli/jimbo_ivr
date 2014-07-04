dd1a60254c93616b47aee24403d7e0436c704711
James Magnarelli <jmagnare@gmail.com>
2014-07-04 18:08:49 -0400

    Fix from phone number fetching

--- request_actions.py
+++ request_actions.py
@@ -8,16 +8,16 @@ class RequestActions(object):
 
     @staticmethod
     def notify_punters(request, section):
-        from_number = request.values.get('FROM', '')
-        msg = "Someone from your IVR wants to go to Punter's. Oh, Joy! Their phone number is {0}, and" \
+        from_number = request.values.get('From', '')
+        msg = "Someone from your IVR wants to go to Punter's. Oh, Joy! Their phone number is {0}, and " \
               "their section was {1}".format(from_number, section.name)
         message = client.messages.create(to=james_number, from_=ivr_number, body=msg)
         print message.sid
 
     @staticmethod
     def schedule_callback(request, section):
-        from_number = request.values.get('FROM', '')
-        msg = "Someone from your IVR wants to be called back. Oh, Joy! Their phone number is {0}, and their" \
+        from_number = request.values.get('From', '')
+        msg = "Someone from your IVR wants to be called back. Oh, Joy! Their phone number is {0}, and their " \
               "section was {1}".format(from_number, section.name)
         message = client.messages.create(to=james_number, from_=ivr_number, body=msg)
         print message.sid
